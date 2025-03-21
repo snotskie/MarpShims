1a78b3b6c40d89381812726b702775c31f6982aa
snotskie <snotskie@gmail.com>
2025-03-21 08:15:14 -0500

    Update MarpShims.js

--- MarpShims.js
+++ MarpShims.js
@@ -2,9 +2,9 @@ const MarpShims = (function(){
   const pub = {};
   // usage: create an anchor with href="javascript:MarpShims.toggleTheme()"
   pub.toggleTheme = function(){
-    document.querySelectorAll('svg[data-marpit-svg] > foreignObject > section').forEach(
-      (e) => e.classList.toggle('invert')
-    );
+    document
+      .querySelectorAll('svg[data-marpit-svg] > foreignObject > section')
+      .forEach((e) => e.classList.toggle('invert'));
   };
 
   // usage: call MarpShims.autoTheme() on load
@@ -16,13 +16,14 @@ const MarpShims = (function(){
 
   // usage: call MarpShims.autoScale() on load. See README for css example
   pub.autoScale = function(){
-    if (('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)){ // always on mobile
-      document.querySelectorAll('svg[data-marpit-svg]').forEach(
-        (e) => e.classList.add('fitscale');
-      );
+    if (('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)){
+      document
+        .querySelectorAll('svg[data-marpit-svg]')
+        .forEach((e) => e.classList.add('fitscale'));
     } else {
-      document.querySelectorAll('svg[data-marpit-svg]').forEach(
-        (e) => e.classList.add('fixedscale');
+      document
+        .querySelectorAll('svg[data-marpit-svg]')
+        .forEach((e) => e.classList.add('fixedscale'));
     }
   };
 
