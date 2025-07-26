79ef9052adad996caaafc92995108f9762804fdc
snotskie <snotskie@gmail.com>
2025-07-26 09:18:55 -0500

    woops

--- MarpShims.js
+++ MarpShims.js
@@ -212,7 +212,7 @@ const MarpShims = (function(){
         target_element.setAttribute("id", "@"+e.innerText.toLowerCase().replaceAll(/[^a-z]/g, ""));
         target_element.style.position = "absolute";
         target_element.style.top = 0;
-        section.appendChild(target_element);
+        section.after(target_element);
 
         records.push({
           anchor: entry_a,
@@ -240,7 +240,7 @@ const MarpShims = (function(){
       target_element.setAttribute("id", "@"+e.innerText.toLowerCase().replaceAll(/[^a-z]/g, ""));
       target_element.style.position = "absolute";
       target_element.style.top = 0;
-      section.appendChild(target_element);
+      section.after(target_element);
 
       const html = e.innerHTML;
       e.innerHTML = "";
